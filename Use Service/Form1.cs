73a62720b63f3a14e6beb85c7b4a8c2023c7fb7e
Богдан Полыгалов <bapolygalov@edu.hse.ru>
2022-06-06 23:01:46 +0500

    Добавьте файлы проекта.

---
+++ Use Service/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Use_Service.ServiceReference1;
+
+namespace Use_Service
+{
+    public partial class Form1 : Form
+    {
+        Service1Client a = new Service1Client();
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double w))
+            {
+                if (textBox1.Text == "" || textBox1.Text == " ")
+                {
+                    textBox1.Text = "0";
+                    label2.Text = $"Из фаренгейт в цельсии: {a.FahrenheitToCelsius(double.Parse(textBox1.Text))}";
+                }
+                else
+                {
+                    label2.Text = $"Из фаренгейт в цельсии: {a.FahrenheitToCelsius(double.Parse(textBox1.Text))}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели не число!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double w))
+            {
+                if (textBox1.Text == "" || textBox1.Text == " ")
+                {
+                    textBox1.Text = "0";
+                    label2.Text = $"Из цельсий в фаренгейты: {a.CelsiusToFahrenheit(double.Parse(textBox1.Text))}";
+                }
+                else
+                {
+                    label2.Text = $"Из цельсий в фаренгейты: {a.CelsiusToFahrenheit(double.Parse(textBox1.Text))}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели не число!");
+            }
+        }
+    }
+}
